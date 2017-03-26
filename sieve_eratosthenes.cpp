ce254ca791da531208aeb525721c0e56c4985bf5
Krunal Shah <krunal-shah@users.noreply.github.com>
2017-03-26 21:52:07 +0530

    Create sieve_eratosthenes.cpp

---
+++ sieve_eratosthenes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+using namespace std;
+
+void find_primes(bool arr[], int n)
+{
+	for(int i=0; i<=n; i++)
+	{
+		arr[i] = false;
+	}	
+	for(int i=2; i<=sqrt(n); i++)
+	{
+		if(arr[i] == false)
+		{	
+			int multiple = 2*i;
+			for(; multiple <= n; multiple += i)
+			{
+				arr[multiple] = true;
+			}
+		}
+	}
+}
+
+
+int main(int argv, char* args[])
+{
+	int n;
+	cin >> n;
+	bool arr[n];
+	find_primes(arr, n);
+}
